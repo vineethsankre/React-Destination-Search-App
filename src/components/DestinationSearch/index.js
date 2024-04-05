e037bc798431047eddd17db04f2b5268c313aca6
vineethsankre <lsvineethsankre@gmail.com>
2024-04-05 14:57:33 +0000

    Static website created

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,7 +1,38 @@
 import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
 import './index.css'
 
-const DestinationSearch = props => {
-  const {destinationsList} = props
-  
+class DestinationSearch extends Component {
+  render() {
+    const {destinationsList} = this.props
+    return (
+      <div className="app-container">
+        <div className="destinations-search-container">
+          <h1 className="heading">Destination Search</h1>
+          <div className="search-input-container">
+            <input
+              type="search"
+              className="search-input"
+              placeholder="Search"
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              alt="search icon"
+              className="search-icon"
+            />
+          </div>
+          <ul className="destinations-list">
+            {destinationsList.map(eachDestination => (
+              <DestinationItem
+                key={eachDestination.id}
+                destinationDetails={eachDestination}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default DestinationSearch
